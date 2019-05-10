0ce3661c19073609b949babf4d9546a984452b21
tuzkimo <tuzkimo@gmail.com>
2019-05-10 09:17:06 +0800

    init

---
+++ src/main/java/cn/tuzkimo/springscratch/SpringScratchApplication.java
@@ -0,0 +1,13 @@
+package cn.tuzkimo.springscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScratchApplication.class, args);
+	}
+
+}
